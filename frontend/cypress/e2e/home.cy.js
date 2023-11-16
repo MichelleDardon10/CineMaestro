0a3cadeaee806616b64541a42c41df1c90187d32
paco <103969393+PacoAcun@users.noreply.github.com>
2023-11-16 17:20:09 -0600

    system testing
    leer readme

--- frontend/cypress/e2e/home.cy.js
+++ frontend/cypress/e2e/home.cy.js
@@ -1,24 +1,46 @@
 
 describe('Prueba del componente Home', () => {
   beforeEach(() => {
+    cy.visit('http://localhost:5173/login');
+    cy.get('[id=user]').type('testuser');
+    cy.get('[id=password]').type('testpassword');
+    cy.get('.iniciar').first().click();
+
+    cy.url().should('eq', 'http://localhost:5173/');
+
     cy.intercept('GET', 'http://localhost:5174/auth/check').as('authCheck');
     cy.intercept('GET', 'http://localhost:5174/movies').as('getMovies');
     cy.visit('http://localhost:5173');
     cy.wait(['@authCheck', '@getMovies']);
   });
 
-  it('debería mostrar al menos una película', () => {
+  it('should display at least one movie', () => {
     cy.get('.movie-card').should('be.visible');
   });
 
-  it('debería marcar/desmarcar una película como vista', () => {
-    cy.get('.movie-card').first().as('primeraPelicula');
+  it('should mark a movie as viewed', () => {
+    cy.get('.movie-card').first().as('firstMovie');
 
-    cy.get('@primeraPelicula').find('.buttons button').eq(1).click();
-    cy.get('@primeraPelicula').find('.buttons button').eq(1).should('contain.text', 'Desmarcar como vista');
+    cy.get('@firstMovie').find('.buttons button').eq(1).should('contain.text', 'Marcar como vista');
+    cy.get('@firstMovie').find('.buttons button').eq(1).click();
+  });
 
-    cy.get('@primeraPelicula').find('.buttons button').eq(1).click();
-    cy.get('@primeraPelicula').find('.buttons button').eq(1).should('contain.text', 'Marcar como vista');
+  it('should unmark a movie as viewed', () => {
+
+    cy.get('.movie-card').first().as('firstMovie');
+
+    cy.get('@firstMovie').find('.buttons button').eq(1).should('contain.text', 'Desmarcar como vista');
+    cy.get('@firstMovie').find('.buttons button').eq(1).click();
+  });
+
+  it('should delete a movie', () => {
+    cy.get('.movie-card').first().as('firstMovie');
+
+    cy.get('@firstMovie').invoke('attr', 'key').then((id) => {
+      cy.get('@firstMovie').contains('Borrar').click();
+
+      cy.get('.movies-list').should('not.contain', id);
+    });
   });
 });
 
---
+++ frontend/cypress/e2e/navbar.cy.js
@@ -0,0 +1,29 @@
+
+describe('Navbar Component', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:5173/login');
+        cy.get('[id=user]').type('testuser');
+        cy.get('[id=password]').type('testpassword');
+        cy.get('.iniciar').first().click();
+    
+        cy.url().should('eq', 'http://localhost:5173/');
+    });
+  
+    it('should render menu items based on authentication status', () => {
+      cy.get('.navbar-menu').should('exist');
+      cy.get('.navbar-menu').contains('Añadir Pelicula').should('exist');
+      cy.get('.navbar-menu').contains('Name movies').should('exist');
+      cy.get('.navbar-menu').contains('Playlist').should('exist');
+      cy.get('.navbar-menu').contains('Perfil').should('exist');
+    });
+    
+    it('should display the "Cerrar sesión" button when authenticated', () => {
+        cy.get('.menu-item').should('be.visible').and('contain.text', 'Cerrar sesión');
+    });
+    
+    it('should render "Login" button after "Cerrar sesión" button is clicked', () => {
+        cy.get('.menu-item').contains('Cerrar sesión').click();
+        cy.get('.menu-item').contains('Cerrar sesión').should('not.exist');
+        cy.get('.navbar-menu').contains('Login').should('exist');
+    });
+  });
