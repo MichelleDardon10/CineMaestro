415ea27beed53a793d7311ae320554ea31ef69b7
miche <michelle.dardon@ufm.edu>
2023-11-17 00:00:35 -0600

    prueba node_modules en git ignore

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+
+console.log('Configuración de Jest cargada correctamente.');
+
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
+  
+};
